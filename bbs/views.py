bc662821fde16aba18052bb6b8817d86267e8692
Shinichi TAKATSUKA <pgstaka@gmail.com>
2025-01-08 02:36:37 +0000

    いったん保存

--- bbs/views.py
+++ bbs/views.py
@@ -1,5 +1,8 @@
 from django.views import generic
 from .models import Article
+from django.urls import reverse_lazy
+from django.shortcuts import render
+from .forms import SearchForm  # forms.pyからSearchFormをインポート
 
 
 # IndexViewクラスを作成
@@ -20,3 +23,31 @@ class CreateView(generic.CreateView):
     model = Article
     template_name = 'bbs/create.html'
     fields = '__all__'
+
+
+# UpdateViewクラスを作成
+class UpdateView(generic.UpdateView):
+    model = Article
+    template_name = 'bbs/create.html'
+    fields = '__all__'
+
+
+# DeleteViewクラスを作成
+class DeleteView(generic.edit.DeleteView):
+    model = Article
+    template_name = 'bbs/delete.html'
+    success_url = reverse_lazy('bbs:index')
+
+
+# search関数を作成
+def search(request):
+    articles = None
+    searchform = SearchForm(request.GET)
+
+    # Formに正常なデータがあれば
+    if searchform.is_valid():
+        # queryにフォームが持っているクエリを代入
+        query = searchform.cleaned_data['query']
+        # クエリを含むレコードをfilterメソッドで取り出し、articlesに代入
+        articles = Article.objects.filter(content__icontains=query)
+        return render(request, 'bbs/results.html', {'articles': articles, 'searchform': searchform})
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello.apps.HelloConfig',  # helloアプリケーションを追加
     'bbs.apps.BbsConfig',  # bbsアプリケーションを追加
+    'django_bootstrap5',  # django-bootstrap5を追加
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bbs.context_processors.search_form',  # bbsアプリケーションのcontext_processors.pyを追加
             ],
         },
     },
